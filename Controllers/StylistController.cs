7257acefeedc7d836a12d35339c75be01a14fd72
Noah Cowan <nsncowan@gmail.com>
2023-03-09 16:18:08 -0800

    add edit view/route

--- Controllers/StylistController.cs
+++ Controllers/StylistController.cs
@@ -41,5 +41,19 @@ namespace HairSalon.Controllers
                                 .FirstOrDefault(stylist => stylist.StylistId == id);
     return View(thisStylist);
     }
+
+    public ActionResult Edit(int id)
+    {
+      Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisStylist);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Stylist stylist)
+    {
+      _db.Stylists.Update(stylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }

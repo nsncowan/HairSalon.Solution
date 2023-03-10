54cfbbd4e8c58457c93500350603608a181458de
Noah Cowan <nsncowan@gmail.com>
2023-03-09 17:18:12 -0800

    add client Index/route

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class ClientController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public ClientController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients
+                              .Include(client => client.Stylist)
+                              .ToList();
+      return View(model);
+    }
+
+    
+  }
+}

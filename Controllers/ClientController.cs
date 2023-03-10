19e1e6f6f9b7211bb9d558a518755bcb5bcdcb70
Noah Cowan <nsncowan@gmail.com>
2023-03-09 17:52:05 -0800

    add client details view/route

--- Controllers/ClientController.cs
+++ Controllers/ClientController.cs
@@ -41,5 +41,12 @@ namespace HairSalon.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult Details(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      return View(thisClient);
+    }
+
   }
 }
